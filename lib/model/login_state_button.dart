7e06d873664d124310ea7a15b627461baac963b0
Raveesh Agarwal <raveeshr503@gmail.com>
2019-03-08 13:37:23 +0530

    login state button

--- lib/model/login_state_button.dart
+++ lib/model/login_state_button.dart
@@ -9,8 +9,39 @@ class LoginStateButton extends StatelessWidget {
       {Key key, @required this.loginState, @required this.changeLoginState})
       : super(key: key);
 
+  LoginState getNewLoginState() {
+    LoginState newState;
+    switch (loginState) {
+      case LoginState.loggedOut:
+        newState = LoginState.loggedIn;
+        break;
+      case LoginState.loggedIn:
+        newState = LoginState.loggedInPro;
+        break;
+      case LoginState.loggedInPro:
+        newState = LoginState.loggedOut;
+        break;
+    }
+    return newState;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return FlatButton(
+      onPressed: () {
+        changeLoginState(getNewLoginState());
+      },
+      child: loginState == LoginState.loggedInPro
+          ? Icon(
+              Icons.star,
+              size: 25,
+            )
+          : Text(
+              loginState == LoginState.loggedOut
+                  ? "LOG In"
+                  : "Subscribe to Pro",
+              style: TextStyle(fontSize: 25),
+            ),
+    );
   }
 }
